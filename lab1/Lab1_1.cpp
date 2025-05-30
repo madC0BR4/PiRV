e1f690b8ab84180e5d5f0954b0e8d76a9d8b67cf
Evan <54749606+madC0BR4@users.noreply.github.com>
2025-05-30 14:09:09 +0300

    лаба 1

---
+++ lab1/Lab1_1.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+
+void multiply_matrix(int start, int end, int n, vector < vector <int> > m1, vector < vector <int> > m2) {
+    vector < vector <long> > res(end - start, vector <long>(n));
+    for (int i = start; i < end; i++) {
+        for (int j = 0; j < n; j++) {
+            res[i - start][j] = 0;
+            for (int k = 0; k < n; k++) {
+                res[i - start][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    // for (int i = 0; i < end-start; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         printf("%d\t", res[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+}
+
+void treads_multiply(int n, int trd) {
+    vector < vector <int> > m1(n, vector <int>(n));
+    vector < vector <int> > m2(n, vector <int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            m1[i][j] = rand() % 100;
+            m2[i][j] = rand() % 100;
+        }
+    }
+    vector <int> lines(trd);
+    for (int i = 0; i < trd; i++) {
+        lines[i] = n / trd;
+    }
+    int s = n % trd;
+    for (int i = 0; i < s; i++) {
+        lines[i] += 1;
+    }
+    vector <int> lines_new(trd + 1, 0);
+    for (int i = 1; i < trd + 1; i++) {
+        lines_new[i] = lines_new[i - 1] + lines[i - 1];
+    }
+
+    vector<thread> threads;
+    for (int i = 0; i < trd; i++) {
+        threads.emplace_back(multiply_matrix, lines_new[i], lines_new[i + 1], n, m1, m2);
+    }
+    for (auto& t : threads) t.join();
+
+    // printf("matx_1\n");
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         printf("%d\t", m1[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("matx_2\n");
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         printf("%d\t", m2[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+}
+
+void single_tread(int n) {
+    vector < vector <long> > res(n, vector <long>(n));
+    vector < vector <int> > m1(n, vector <int>(n));
+    vector < vector <int> > m2(n, vector <int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            m1[i][j] = rand() % 100;
+            m2[i][j] = rand() % 100;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            res[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                res[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int thread_number = 4;
+    int N[3] = { 100, 500, 1000 };
+    printf("name        N        time\n");
+    printf("--------------------------\n");
+    for (int i = 0; i < 3; i++) {
+        auto start = chrono::steady_clock::now();
+        treads_multiply(N[i], thread_number);
+        auto stop = chrono::steady_clock::now();
+        chrono::duration<double> p = stop - start;
+
+        start = chrono::steady_clock::now();
+        single_tread(N[i]);
+        stop = chrono::steady_clock::now();
+        chrono::duration<double> s = stop - start;
+        printf("single     %d     %4.4f s\n", N[i], s.count());
+        printf("parallel   %d     %4.4f s\n", N[i], p.count());
+    }
+
+    return 0;
+}
---
+++ lab1/Lab1_2.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <mutex>
+#include <atomic>
+using namespace std;
+
+mutex mtx;
+const int transactions_number = 10000;
+int client_number = 4;
+vector <int> BANK_ACCOUNTS = { 100, 100, 100, 100 };
+vector <int> bank_accounts;
+vector < vector <int> > TRANSACTIONS(transactions_number, vector <int>(2));
+
+
+void transaction(int trans, int param) {
+    if (param == 1) {
+        bank_accounts[TRANSACTIONS[trans][0]] += TRANSACTIONS[trans][1];
+    }
+    else {
+        mtx.lock();
+        bank_accounts[TRANSACTIONS[trans][0]] += TRANSACTIONS[trans][1];
+        mtx.unlock();
+    };
+}
+
+void do_transactions(int trd, int param) {
+    vector<thread> threads;
+    int counter = 0;
+    while (counter < TRANSACTIONS.size() - trd + 1) {
+        vector<thread> threads;
+        for (int i = counter; i < counter + trd; i++) {
+            threads.emplace_back(transaction, i, param);
+        }
+        counter += trd;
+        for (auto& t : threads) t.join();
+    };
+}
+
+int main() {
+    srand(time(NULL));
+    for (int i = 0; i < transactions_number; i++) {
+        TRANSACTIONS[i][0] = rand() % client_number;
+        TRANSACTIONS[i][1] = rand() % 40 - 20;
+    }
+
+    vector <int> treads_arr = { 2, 4, 8 };
+
+    for (int tread = 0; tread < 3; tread++) {
+        cout << "Number of threads: " << treads_arr[tread] << endl;
+        cout << "---------------------------------------" << endl;
+        cout << "protection" << "    time     " << "result" << endl;
+        cout << "---------------------------------------" << endl;
+
+        bank_accounts = BANK_ACCOUNTS;
+
+        auto start = chrono::steady_clock::now();
+        do_transactions(treads_arr[tread], 1);
+        auto stop = chrono::steady_clock::now(); 
+        chrono::duration<double> t_no = stop - start;
+        printf("None        %3.4f s  ",t_no.count());
+        for (int i = 0; i < client_number; i++) {
+            cout << bank_accounts[i] << " ";
+        }
+        cout << endl;
+
+        for (int i = 0; i < client_number; i++) {
+            atomic <int> a = BANK_ACCOUNTS[i];
+            bank_accounts[i] = a;
+        }
+        start = chrono::steady_clock::now();
+        do_transactions(treads_arr[tread], 1);
+        stop = chrono::steady_clock::now();
+        chrono::duration<double> t_atomic = stop - start;
+        printf("Atomic      %3.4f s  ", t_no.count());
+        for (int i = 0; i < client_number; i++) {
+            cout << bank_accounts[i] << " ";
+        }
+        cout << endl;
+
+        bank_accounts = BANK_ACCOUNTS;
+        start = chrono::steady_clock::now();
+        do_transactions(treads_arr[tread], 2);
+        stop = chrono::steady_clock::now();
+        chrono::duration<double> t_mutex = stop - start;
+        printf("Mutex       %3.4f s  ", t_no.count());
+        for (int i = 0; i < client_number; i++) {
+            cout << bank_accounts[i] << " ";
+        }
+        cout << endl;
+        cout << "=======================================" << endl;
+        cout << endl;
+        bank_accounts.clear();
+    }
+
+    return 0;
+}
