bc2f47127f1c6186006ac7294c337b70dad9a9fa
Evan <54749606+madC0BR4@users.noreply.github.com>
2025-06-05 13:33:51 +0300

    Лаба2

---
+++ lab2/Lab2_1.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <thread>
+#include <queue>
+#include <random>
+#include <semaphore>
+#include <chrono>
+#include <vector>
+#include <mutex>
+using namespace std;
+
+struct Task {
+	int id;
+	int priority;
+	int time;
+	bool operator<(const Task& other) const {
+		return priority > other.priority; 
+	}
+};
+
+priority_queue<Task> task_queue;
+counting_semaphore<3> servers(3);
+mutex output_mutex;
+random_device rd;
+mt19937 gen(rd());
+
+void add_task(int id, int priority) {
+	uniform_int_distribution<> dis(1, 5);
+		Task task = { id, priority, dis(gen) };
+	task_queue.push(task);
+	lock_guard<mutex> guard(output_mutex);
+	cout << "Task " << id << " with priority " << priority << " added to the queue.\n";
+}
+
+void process_task(Task task, int thread_id) {
+	servers.acquire();
+	this_thread::sleep_for(chrono::seconds(task.time));
+	{
+		lock_guard<mutex> guard(output_mutex);
+		cout << "Task " << task.id << " (priority " << task.priority << ") completed after "
+		<< task.time << " seconds. Server: " << thread_id + 1 << endl;
+	}
+	servers.release();
+}
+
+void process_tasks(int thread_id) {
+	while (!task_queue.empty()) {
+		Task task = task_queue.top();
+		task_queue.pop();
+		process_task(task, thread_id);
+	}
+}
+
+int main() {
+	for (int i = 0; i < 10; ++i) {
+		uniform_int_distribution<> dis(1, 5);
+		add_task(i+1, dis(gen));
+	}
+
+	vector<thread> threads;
+	for (int i = 0; i < 5; ++i) {
+		threads.push_back(thread(process_tasks, i));
+	}
+	for (auto& t : threads) {
+		t.join();
+	}
+	return 0;
+}
---
+++ lab2/Lab2_2.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <thread>
+#include <queue>
+#include <random>
+#include <semaphore>
+#include <chrono>
+#include <vector>
+#include <mutex>
+using namespace std;
+
+mutex mtx;
+
+struct CrossRoad {
+	int id;
+	int green_time_1;
+	int green_time_2;
+	bool side;
+};
+
+struct TrafficQueue {
+	int id;
+	int queue_time;
+	bool emergency_flag;
+};
+
+static const int GREEN_LIGHT_STANDART = 20;
+static const int SIMULATING_TIME = 100;
+vector<CrossRoad> CrossVector(10);
+vector<TrafficQueue> TrafficVector(20);
+
+int genNum(int a, int b) {
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed);
+	uniform_int_distribution<int> distribution(a, b);
+	int random_number = distribution(generator);
+	return random_number;
+}
+
+void CreateCrossRoads() {
+	for (int i = 0; i < 10; i++) {
+		CrossRoad cs = { i, GREEN_LIGHT_STANDART, GREEN_LIGHT_STANDART, false };
+		CrossVector[i] = cs;
+
+		int time = genNum(0, 1);
+		bool flag = false;
+		if (genNum(1, 10) == 3) {
+			flag = true;
+		}
+		TrafficQueue tq = { i, time, flag };
+		TrafficVector[i] = tq;
+
+		time = genNum(0, 1);
+		flag = false;
+		if (genNum(1, 10) == 3) {
+			flag = true;
+		}
+		tq = { i + 10, time, flag };
+		TrafficVector[i + 10] = tq;
+	}
+}
+
+void TrafficGeneration() {
+	for (int i = 0; i < 20; i++) {
+		int time = 0;
+		if (genNum(1, 10) == 3) {
+			time = 1;
+		}
+		bool flag = false;
+		if (genNum(1, 10) == 3) {
+			flag = true;
+		}
+		TrafficVector[i].queue_time += time;
+		TrafficVector[i].emergency_flag = flag;
+	}
+}
+
+void ChangeCrossTime(int i) {
+	if (CrossVector[i].side == false) {
+		//int all_traffic = TrafficVector[i].queue_time + TrafficVector[i + 10].queue_time;
+		if ((TrafficVector[i].queue_time * 100 / CrossVector[i].green_time_1) > 70 ) {
+			CrossVector[i].green_time_1 *= 1.5;
+		}
+		if ((TrafficVector[i].queue_time * 100 / CrossVector[i].green_time_1) < 50) {
+			CrossVector[i].green_time_1 = GREEN_LIGHT_STANDART;
+		}
+	}
+	if (CrossVector[i].side == true) {
+		//int all_traffic = TrafficVector[i].queue_time + TrafficVector[i + 10].queue_time;
+		if ((TrafficVector[i+10].queue_time * 100 / CrossVector[i].green_time_2) > 70) {
+			CrossVector[i].green_time_2 *= 1.5;
+		}
+		if ((TrafficVector[i+10].queue_time * 100 / CrossVector[i].green_time_2) < 50) {
+			CrossVector[i].green_time_2 = GREEN_LIGHT_STANDART;
+		}
+	}
+}
+
+void CheckTraffic(int i) {
+	if (CrossVector[i].side == false) {
+		TrafficVector[i].queue_time -= 1;
+		if (TrafficVector[i].queue_time < 0) {
+			TrafficVector[i].queue_time = 0;
+		}
+	}
+	if (CrossVector[i].side == true) {
+		TrafficVector[i + 10].queue_time -= 1;
+		if (TrafficVector[i + 10].queue_time < 0) {
+			TrafficVector[i + 10].queue_time = 0;
+		}
+	}
+}
+
+void CrossSimulating(int i) {
+	int change_flag_time = CrossVector[i].green_time_1;
+	for (int j = 0; j < SIMULATING_TIME; j++) {
+		TrafficGeneration();
+		if (change_flag_time == 0) {
+			ChangeCrossTime(i);
+			if (CrossVector[i].side == false) {
+				CrossVector[i].side = true;
+				change_flag_time = CrossVector[i].green_time_2;
+			}
+			if (CrossVector[i].side == true) {
+				CrossVector[i].side = false;
+				change_flag_time = CrossVector[i].green_time_1;
+			}
+		}
+		change_flag_time -= 1;
+		CheckTraffic(i);
+		if (j % 5 == 0) {
+			mtx.lock();
+			cout << "Cross: " << CrossVector[i].id << ". Traffic: " << (TrafficVector[i].queue_time * 100 / CrossVector[i].green_time_1) << "%." << endl;
+			mtx.unlock();
+		}
+	}
+}
+
+int main() {
+	CreateCrossRoads(); 
+	vector<thread> threads;
+
+	for (int i = 0; i < 10; ++i) {
+		threads.push_back(thread(CrossSimulating, i));
+	}
+	for (auto& t : threads) {
+		t.join();
+	}
+
+	/*for (int j = 0; j < 5; j++) {
+		TrafficGeneration();
+		for (int i = 0; i < 20; i++) {
+			cout << "Cross: " << TrafficVector[i].id << ", time: " << TrafficVector[i].queue_time << ", emergency - " << TrafficVector[i].emergency_flag << endl;
+		}
+		cout << "----------------------------------------------------" << endl;
+	}*/
+
+	return 0;
+}
